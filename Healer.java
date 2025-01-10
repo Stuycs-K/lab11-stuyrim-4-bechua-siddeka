1b6d6a82fa006672b8e501592fe88bb31a51ee7f
Anzam Siddek <asiddek50@stuy.edu>
2025-01-10 00:44:08 -0500

    Added attack and support

--- Healer.java
+++ Healer.java
@@ -24,4 +24,33 @@ public class Healer extends Adventurer {
     public void setSpecial(int n){
         hpHealed = n;
     }
+
+    public String attack(Adventurer other) {
+        Random random = new Random();
+        int damage = random.nextInt(2) + 2;
+
+        other.applyDamage(damage);
+
+        return getName() + " attacked " + other.getName() + " and dealt " + damage + " damage.";
+    }
+
+    public String support(Adventurer other) {
+        Random random = new Random();
+        int addedHP = random.nextInt(3) + 5;
+
+        other.setHP(getHP()+addedHP);
+        setSpecial(getSpecial()+addedHP);
+
+        return getName() + " restored " + addedHP + " HP for " + other.getName() + ". They need to restore " + (15-getSpecial()) + " HP to unlock their special.";
+    }
+
+    public String support() {
+        Random random = new Random();
+        int addedHP = random.nextInt(3) + 5;
+
+        setHP(getHP()+addedHP);
+        setSpecial(getSpecial()+addedHP);
+
+        return getName() + " restored " + addedHP + " HP for themself. They need to restore " + (15-getSpecial()) + " HP to unlock their special.";
+    }
 }
