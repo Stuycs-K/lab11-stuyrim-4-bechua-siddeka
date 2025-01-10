de18ba79b9a57a42f88e53c4e241ed1897acd27a
anjali814 <abechu60@stuy.edu>
2025-01-09 21:27:42 -0500

    added the private variables

--- Boss.java
+++ Boss.java
@@ -1,3 +1,10 @@
 public class Boss extends Adventurer{
+  private int attackMin = 10;
+  private int attackMax = 12;
+  private int supportMin = 7;
+  private int supportMax = 9;
+  private int specialResource;
+  private int specialMax = 100;
+
   
 }
