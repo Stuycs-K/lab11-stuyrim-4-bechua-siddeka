80819ed0373dc73efaccbce9a4f4c7f28d289160
anjali814 <abechu60@stuy.edu>
2025-01-20 15:47:04 -0500

    added methods

--- Enemy.java
+++ Enemy.java
@@ -1,35 +1,48 @@
-public class Enemy {
-  private String name;
-  private int HP;
-  private int maxHP;
-  private String special;
-  private String specialName;
+import java.util.Random;
+public class Enemy extends Adventurer {
+  private int rage;
 
   public Enemy(String name, int maxHP, String special, String specialName){
-    this.name = name;
-    this.maxHP = maxHP;
-    this.HP = maxHP;
-    this.special = special;
-    this.specialName = specialName;
+    super(name, maxHP, special, specialName);
+    this.rage = 0;
   }
 
   public String getName(){
-        return name;
+      return name;
     }
 
     public int getHP() {
-        return HP;
+      return HP;
     }
 
     public int getMaxHP(){
-        return maxHP;
+      return maxHP;
     }
 
     public String getSpecial() {
-        return special;
+      return special;
     }
 
     public String getSpecialName() {
-        return specialName;
+      return specialName;
+    }
+
+    public void attack(Adventurer target) {
+      Random rng = new Random();
+      int damage = rng.nextInt(5) + 5;
+      System.out.println(this.name + " attacks " + target.getName() + " for " + damage + " damage!");
+
+      target.takeDamage(damage);
+    }
+
+    public void support() {
+      System.out.println(this.name + " provides support to an ally!");
+    }
+
+    public void specialAttack(Adventurer target) {
+      Random rng = new Random();
+      int specialDamage = rng.nextInt(10) + 10;  // Special attack does more damage
+      System.out.println(this.name + " uses special attack " + this.specialName + " on " + target.getName() + " for " + specialDamage + " damage!");
+      target.applyDamage(specialDamage);
     }
 }
